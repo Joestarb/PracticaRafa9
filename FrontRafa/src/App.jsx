909c3f3bf8f8bf861ac3ae28bde5702694a48271
Edgar-MedCor <edgarmen159@gmail.com>
2023-10-29 21:20:00 -0500

    cree el index y el catalogo de recetas

--- FrontRafa/src/App.jsx
+++ FrontRafa/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
+import Catalogo from './pages/Catalogo'
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index/>} />
+          <Route path="Catalogo" element={<Catalogo/>} />
         </Routes>
       </BrowserRouter>
     </div>
